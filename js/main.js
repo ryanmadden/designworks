d60d9c961a1b46d06535c93861125de583ef0597
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2014-10-26 11:50:06 -0500

    Changed loading animation timing

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 $(document).ready(function()
 {
 	//Hexagon loading screen
-	$("#a").fadeTo(500, 1, function() {
-		$("#b").fadeTo(500, 1, function() {
-			$("#c").fadeTo(500, 1, function() {
-				$("#e").fadeTo(500, 1, function() {
-					$("#d").fadeTo(500, 1, function() {
-						$("#f").fadeTo(500, 1, function() {
+	var loadtime = 400;
+	var fadedelay = 800;
+	var fadetime = 900;
+	$("#a").delay(200).fadeTo(loadtime, 1, function() {
+		$("#b").fadeTo(loadtime, 1, function() {
+			$("#c").fadeTo(loadtime, 1, function() {
+				$("#e").fadeTo(loadtime, 1, function() {
+					$("#d").fadeTo(loadtime, 1, function() {
+						$("#f").fadeTo(loadtime, 1, function() {
 							$("#title-wrapper").fadeTo(500, 1, function() {
-								$("#cover").fadeTo(800, 0, function() {
-										$("#cover").css("display", "none");
-	});
-	});
-	});
-	});
-	});
-	});
-	});
+								$("#cover").delay(fadedelay).fadeTo(fadetime, 0, function() {
+									$("#cover").css("display", "none");
+								});
+							});
+						});
+					});
+				});
+			});
+		});
 	});
 	$("#gallery img").click(function()
 	{
