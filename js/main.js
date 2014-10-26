4a1580af4cb1b292b2fcc523515e323a61490178
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2014-10-25 21:44:16 -0500

    Loading page setup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function()
 {
+	$("#a").fadeTo(500, 1, function() {
+		$("#b").fadeTo(500, 1, function() {
+			$("#c").fadeTo(500, 1, function() {
+				$("#e").fadeTo(500, 1, function() {
+					$("#d").fadeTo(500, 1, function() {
+						$("#f").fadeTo(500, 1, function() {
+	});
+	});
+	});
+	});
+	});
+	});
 	$("#gallery img").click(function()
 	{
 		$("#modal-img").attr("src", $(this).attr('src'));
